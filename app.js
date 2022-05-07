e7965f86d3bbb72ac86056fcb0b2d30d23755187
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-05-07 06:48:11 -0500

    displayProducts

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('filters project');
+
+// console.log(products)
+
+let filteredProducts = [...products];
+// console.log(filteredProducts)
+
+const productContainer = document.querySelector('.products-container');
+
+const displayProducts = () => {
+    // if statement
+
+    productContainer.innerHTML = filteredProducts.map(({ id,title,image,price }) => {
+
+        return `<article class="product data-id='${id}'">
+        <img src=${image} class="product-img img" alt="">
+        <footer>
+          <h5 class="product-name">${title}</h5>
+          <span class="product-price">${price}</span>
+        </footer>
+      </article>`
+      
+    }).join('')
+}
+
+displayProducts();
