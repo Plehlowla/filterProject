bbadd78faa64772a84fef1b898c3da939dcd72a1
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-05-07 09:03:13 -0500

    displaybuttons

--- app.js
+++ app.js
@@ -6,41 +6,60 @@ let filteredProducts = [...products];
 
 const productContainer = document.querySelector('.products-container');
 
-const displayProducts = () => {
+// companies.innerHTML = ['all',...new Set(products.map(({ company,id }) => {
+  //   return `<button class="company-btn" data-id="${id}">${company}</button>`
+  // }))].join('')
+  
+  
+  
+  const displayProducts = () => {
     // if statement
     
     if(filteredProducts.length < 1){
       productContainer.innerHTML = `<h6>Sorry, no products matched your search</h6>`;
       return
     }
-
-
+    
+    
     productContainer.innerHTML = filteredProducts.map(({ id,title,image,price }) => {
-
-        return `<article class="product data-id='${id}'">
-        <img src=${image} class="product-img img" alt="">
-        <footer>
-          <h5 class="product-name">${title}</h5>
-          <span class="product-price">${price}</span>
-        </footer>
+      
+      return `<article class="product" data-id='${id}'>
+      <img src=${image} class="product-img img" alt="">
+      <footer>
+      <h5 class="product-name">${title}</h5>
+      <span class="product-price">${price}</span>
+      </footer>
       </article>`
       
     }).join('')
-}
-
-displayProducts();
-
-// Text Filter
-
-const form = document.querySelector('.input-form')
-const searchInput = document.querySelector('.search-input')
-
-form.addEventListener('keyup', () => {
+  }
+  
+  displayProducts();
+  
+  // Text Filter
+  
+  const form = document.querySelector('.input-form')
+  const searchInput = document.querySelector('.search-input')
+  
+  form.addEventListener('keyup', () => {
     const inputValue = searchInput.value;
     // console.log(inputValue)
-
+    
     filteredProducts = products.filter((product) => {
-        return product.title.toLowerCase().includes(inputValue);
+      return product.title.toLowerCase().includes(inputValue);
     })
     displayProducts();
-});
+  });
+  
+  // Filter Buttons
+  
+  const companiesDOM = document.querySelector('.companies');
+
+  const displayButtons = () => {
+    const buttons = ['all',...new Set(products.map((product) => product.company)),]
+    companiesDOM.innerHTML = buttons.map((company) => {
+      return `<button class="company-btn" data-id="${company}">${company}</button>`
+    }).join('')
+  }
+  
+  displayButtons();
